290a465324bf1d039314bff8a165112b6cc88098
unknown <hamiltonjlewis@gmail.com>
2020-02-09 17:33:12 -0600

    added social information

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
+				<h1>
+					Follow us on social and be the first to know about updates and new
+					content
+				</h1>
+				<a
+					style={{ textDecoration: 'none', color: 'white' }}
+					href='https://www.instagram.com/479podcast'>
+					Instagram
+				</a>
+				<a
+					style={{ textDecoration: 'none', color: 'white' }}
+					href='https://twitter.com/479Podcast?s=20'>
+					Twitter
+				</a>
 			</header>
 		</div>
 	)

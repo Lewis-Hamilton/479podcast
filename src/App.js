5fe1119534ce64c0d682854581a923d3d464d444
unknown <hamiltonjlewis@gmail.com>
2020-02-12 23:25:47 -0600

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import logo from './logo.png'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<h1>
-					Follow us on social and be the first to know about updates and new
-					content
-				</h1>
-				<a
-					style={{ textDecoration: 'none', color: 'white' }}
-					href='https://www.instagram.com/479podcast'>
-					Instagram
-				</a>
-				<a
-					style={{ textDecoration: 'none', color: 'white' }}
-					href='https://twitter.com/479Podcast?s=20'>
-					Twitter
-				</a>
-			</header>
-		</div>
+		// <div className='App'>
+		// 	<header className='App-header'>
+		// 		<img src={logo} className='App-logo' alt='logo' />
+		// 		<h1>
+		// 			Follow us on social and be the first to know about updates and new
+		// 			content
+		// 		</h1>
+		// 		<a
+		// 			style={{ textDecoration: 'none', color: 'white' }}
+		// 			href='https://www.instagram.com/479podcast'>
+		// 			Instagram
+		// 		</a>
+		// 		<a
+		// 			style={{ textDecoration: 'none', color: 'white' }}
+		// 			href='https://twitter.com/479Podcast?s=20'>
+		// 			Twitter
+		// 		</a>
+		// 	</header>
+		// </div>
+		<Home></Home>
 	)
 }
 
